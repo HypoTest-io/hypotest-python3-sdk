5153cd94de7dff26bc7f1076459aa4b2db568bca
lior brafman <liorb87@gmail.com>
2022-04-27 18:47:32 +0300

    better toml file

--- hypotest_python3_sdk/__init__.py
+++ hypotest_python3_sdk/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '1.0.2'
+from config import config
+from routing import experiment
+from hypotest import kpi_event, match_user_visitor
+
+__all__ = ['config', 'experiment', 'kpi_event', 'match_user_visitor']
+__version__ = '1.0.4'
--- tests/test_hypotest_python3_sdk.py
+++ tests/test_hypotest_python3_sdk.py
@@ -2,4 +2,4 @@ from hypotest_python3_sdk import __version__
 
 
 def test_version():
-    assert __version__ == '1.0.2'
+    assert __version__ == '1.0.4'
